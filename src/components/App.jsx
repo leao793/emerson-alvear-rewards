1db360c4dc7f307d693ef865c65b99d518d3df92
Emerson Alvear <leao793@hotmail.com>
2021-10-14 14:03:03 -0500

    Configuracion Github pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from "../components/Header";
 import Shop from "../pages/Shop";
-
+import Redeems from "../pages/Redeems";
+import GetCoins from "../pages/GetCoins";
+import Header from "../components/Header";
 
 import { UserProvider } from "../components/Context/UserContext";
 import { FilterProvider } from "../components/Context/FilterContext";
@@ -18,6 +19,8 @@ function App() {
 						<Header />
 						<Switch>
 							<Route exact path="/" component={Shop} />
+							<Route path="/redeems" component={Redeems} />
+							<Route path="/getcoins" component={GetCoins} />
 						</Switch>
 					</FilterProvider>
 				</ProductProvider>
